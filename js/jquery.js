644e3969a89005138dd23ef6345bf1e7ed721032
ProfeDickison <38512688+ProfeDickison@users.noreply.github.com>
2018-07-20 15:14:43 -0600

    Create jquery.js

---
+++ js/jquery.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+   console.log("Hello World!");
+    
+    $(".centered").css("display", "none");
+    
+    // Example FadeIn Method Applied to Navs
+    $(".centered").fadeIn(5000); 
+    
+    $("nav").css("display", "none");
+    
+    $(".icons").css("display", "none");
+    $(".icons").fadeIn(5000);
+    
+    // Example FadeIn Method Applied to Navs
+    $("nav").fadeIn(5000); 
+});
